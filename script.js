15485bd13ad219103de4a08dda25e74e99d2f815
p <m>
2017-09-30 17:08:50 +0200

    loop

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-funfunction drawTree(height) {
-	for(i=0; i<6; i++){
-      var star = '';
-  		for(var j = 0; j < height; j++) {
-    		star += '*';
+function drawTree(height) {
+    for (var i = 0; i < height; i++) {
+        var star = '';
+        for (var j = 0; j <= i; j++) {
+            star += '*';
         }
+        console.log(star);
     }
-    console.log(star);
 }
-drawTree(1);
-drawTree(2);
-drawTree(3);
 drawTree(4);
-drawTree(5);
-drawTree(6);
-drawTree(7); 
