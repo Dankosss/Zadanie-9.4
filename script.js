79f7c5215ec16db7b350e9bd54c1a3e82b5944d4
m <m>
2017-09-30 14:48:03 +0200

    loops

---
+++ script.js
@@ -0,0 +1,16 @@
+funfunction drawTree(height) {
+	for(i=0; i<6; i++){
+      var star = '';
+  		for(var j = 0; j < height; j++) {
+    		star += '*';
+        }
+    }
+    console.log(star);
+}
+drawTree(1);
+drawTree(2);
+drawTree(3);
+drawTree(4);
+drawTree(5);
+drawTree(6);
+drawTree(7); 
